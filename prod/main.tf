4313f2b0fb55edb1ff8181cbc3b9b5ce63de1de3
EC2 Default User <ec2-user@ip-172-31-90-6.ec2.internal>
2023-09-15 01:24:11 +0000

    change property2

--- prod/main.tf
+++ prod/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
     default_tags{
         tags = {
             Owner = "Breno Moreira"
-            Env = "Producao"
+            Env = "Producao2"
             Class = "CLC10"
         }
     }
